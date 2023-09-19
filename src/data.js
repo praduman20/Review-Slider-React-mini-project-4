f86138add1bf507134ccae78b9b2847c5cb23d39
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-09-19 21:52:41 +0530

    Build Commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Outstanding organizational abilities have streamlined our office operations, creating a productive and harmonious work environment.",
+  },
+  {
+    id: 2,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "A valued team member, consistently contributing to our workplace camaraderie and fostering a positive, collaborative atmosphere.",
+  },
+  {
+    id: 3,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Exceptional creativity and design skills have consistently elevated our products, making them stand out in the market.",
+  },
+  {
+    id: 4,
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "susan andersen",
+    title: "developer",
+    quote:
+      "Remarkable coding expertise has resulted in top-notch software solutions, solving complex problems with precision and ingenuity.",
+  },
+];
+
+export default people;
